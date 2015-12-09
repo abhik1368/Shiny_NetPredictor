6d396b0162f6a69b8a21eb7696d69fa3479a403f
Abhik Seal <abhik1368@gmail.com>
2015-12-09 17:20:43 -0500

    Update license

--- server.R
+++ server.R
@@ -1,3 +1,16 @@
+# Copyright (C) 2015 Abhik Seal <abhik1368@gmail.com>
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details. 
+#################################################################################
+## Load the libraries
+
 library(netpredictor)
 library(igraph)
 library(gdata)
@@ -9,7 +22,7 @@ library(data.table)
 source('global.R')
 library(networkD3)
 library(visNetwork)
-
+#################################################################################
 
 shinyServer( function(input, output,session) {
     
