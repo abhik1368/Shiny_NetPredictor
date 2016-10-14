f2279b2c938bb8a59dece256b47e25e0bc66225f
Abhik Seal <abhik1368@gmail.com>
2016-10-13 22:58:19 -0500

    Update global.R

--- global.R
+++ global.R
@@ -1,13 +1,8 @@
-# Copyright (C) 2015 Abhik Seal <abhik1368@gmail.com>
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as
-# published by the Free Software Foundation, either version 3 of the
-# License, or (at your option) any later version.
-#
+# Copyright (C) 2016 Abhik Seal <abhik1368@gmail.com>
+# This program CC BY-NC-SA 3.0 license.
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details. 
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 #################################################################################
 ## INSTALLATION OF PACKAGES FIRST
 # list.of.packages <- c("devtools","DBI","RSQLite","shinyBS","doParallel","foreach","netpredictor","lpbrim","shinysky","shinyjs","shinythemes","reshape2","rlist","htmltools","igraph","gdata","shiny","data.table","visNetwork","DBI","RSQLite") # replace xx and yy with package names
